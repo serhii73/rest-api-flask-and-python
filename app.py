b9665dbc73af30e22fe9b9db2badaf48138263f4
serhii <aserhii@protonmail.com>
2018-11-23 15:34:39 +0200

    Creating our application endpoints

--- app.py
+++ app.py
@@ -2,10 +2,37 @@ from flask import Flask
 
 app = Flask(__name__)
 
+stores = [{"name": "My store", "items": [{"name": "my item", "price": 11}]}]
+
 
 @app.route("/")
 def home():
     return "Hello, world!"
+
+
+@app.route("/store", methods=["POST"])
+def create_store():
+    pass
+
+
+@app.route("/store/<string:name>")
+def get_store(name):
+    pass
+
+
+@app.route("/store")
+def get_stores():
+    pass
+
+
+@app.route("/store/<string:name>/item", methods=["POST"])
+def create_item_in_store(name):
+    pass
+
+
+@app.route("/store/<string:name>/item")
+def get_item_in_store(name):
+    pass
 
 
 app.run(port=5000)
