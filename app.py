5711ba3582c747a7ee5221ee31f5c9dcb0013648
serhii <aserhii@protonmail.com>
2018-12-04 21:03:22 +0200

    Add code

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
 from flask import Flask, request
-
+from flask_jwt import JWT, jwt_required
 from flask_restful import Api, Resource
 
+from security import authenticate, identity
+
 app = Flask(__name__)
+app.secret_key = "serhii"
 api = Api(app)
+
+jwt = JWT(app, authenticate, identity)
 
 items = []
 
 
 class Item(Resource):
+    @jwt_required()
     def get(self, name):
         item = next(filter(lambda x: x["name"] == name, items), None)
         return {"item": None}, 200 if item else 404
